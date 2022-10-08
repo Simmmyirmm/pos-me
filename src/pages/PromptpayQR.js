55bcc966cf074f9c836c73c4f7a231bef5b2239a
Simmmyirmm <viperrkosim@gmail.com>
2022-10-08 23:13:52 +0700

    Update QR

--- src/pages/PromptpayQR.js
+++ src/pages/PromptpayQR.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import styles from "./PromptpayQR.module.css";
 import QRCode from "qrcode.react";
+import photo from "../photo/thai_qr.png";
+const generatePayload = require("promptpay-qr");
 
 const PromptpayQR = function () {
   const [phoneNumber, setPhoneNumber] = useState("012-345-6789");
-  const [amount, setAmount] = useState(1.0);
+  const [amount, setAmount] = useState(788);
   const [qrCode, setqrCode] = useState("sample");
+  const [showQR, setShowQR] = useState(false);
+
   return (
     <div className={styles.section_QR}>
       <div className={styles.container}>
@@ -13,10 +17,17 @@ const PromptpayQR = function () {
           <p className={styles.price_total_text}>Total</p>
           <p className={styles.price_total}>... ฿</p>
         </div>
-        <div className={styles.qr_box}>
-          <QRCode value={qrCode} />
+        <div className={styles.u_center}>
+          <div className={styles.qr_box}>
+            <img src={photo} alt="Promptpay" className={styles.qr_img} />
+            <div className={styles.qr_card}>
+              <QRCode value={qrCode} size={200} />
+              <p className={styles.qr_phone_num}>{phoneNumber}</p>
+              <p className={styles.qr_amount}>{amount} บาท</p>
+            </div>
+          </div>
+          <button className={styles.button}>เสร็จสิ้น</button>
         </div>
-        <button className={styles.button}>เสร็จสิ้น</button>
       </div>
     </div>
   );
